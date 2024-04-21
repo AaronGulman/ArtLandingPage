3000aeb68b2072d00cd397df967e72f7648f0b49
AaronGulman <81993286+AaronGulman@users.noreply.github.com>
2024-04-20 23:42:08 -0500

    Establish MongoDB Connection

---
+++ server/db/connection.js
@@ -0,0 +1,30 @@
+import {MongoClient , ServerApiVersion } from "mongodb"
+
+
+const uri = process.env.ATLAS_URI || 5050
+
+const client = new MongoClient(uri, {
+	serverApi:{
+		version: ServerApiVersion.v1,
+		strict: true,
+		deprecationErrors: true,
+	}
+})
+
+
+try{
+	await client.connect();
+
+	await client.db('admin').command({ping: 1})
+	console.log("DB connection - SUCCESS!")
+
+
+}catch(err){
+	console.log('DB connection - FAILED' , err)
+}
+
+
+let db = client.db('name of your db') // IF YOU HAVE NOT CREATED A DB , WORRY NOT ! THIS WILL AUTOMATICALLY CREATE ONE FOR YOU!
+
+
+export default db;
