512bc754313954618d955bc996a500bef6f729de
AaronGulman <81993286+AaronGulman@users.noreply.github.com>
2024-04-22 19:55:19 -0500

    Properly format connection.js

--- server/db/connection.js
+++ server/db/connection.js
@@ -1,30 +1,24 @@
-import {MongoClient , ServerApiVersion } from "mongodb"
+import { MongoClient, ServerApiVersion } from "mongodb";
 
-
-const uri = process.env.ATLAS_URI || 5050
+const uri = process.env.ATLAS_URI || 5050;
 
 const client = new MongoClient(uri, {
-	serverApi:{
-		version: ServerApiVersion.v1,
-		strict: true,
-		deprecationErrors: true,
-	}
-})
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
 
+try {
+  await client.connect();
 
-try{
-	await client.connect();
-
-	await client.db('admin').command({ping: 1})
-	console.log("DB connection - SUCCESS!")
-
-
-}catch(err){
-	console.log('DB connection - FAILED' , err)
+  await client.db("admin").command({ ping: 1 });
+  console.log("DB connection - SUCCESS!");
+} catch (err) {
+  console.log("DB connection - FAILED", err);
 }
 
-
-let db = client.db('name of your db') // IF YOU HAVE NOT CREATED A DB , WORRY NOT ! THIS WILL AUTOMATICALLY CREATE ONE FOR YOU!
-
+let db = client.db("art_landing_page"); // IF YOU HAVE NOT CREATED A DB , WORRY NOT ! THIS WILL AUTOMATICALLY CREATE ONE FOR YOU!
 
 export default db;
