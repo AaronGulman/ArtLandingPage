a670d36d76e102d3aca3c447b2012d44e542f725
AaronGulman <81993286+AaronGulman@users.noreply.github.com>
2024-04-22 19:57:32 -0500

    Update server setup to include route handling for orders

--- server/server.js
+++ server/server.js
@@ -1,13 +1,13 @@
-import express from "express"
-import cors from 'cors'
-//import router here
+import express from "express";
+import cors from "cors";
+import orders from "./router/router.js";
 
 const server = express();
 const PORT = process.env.PORT || 5050;
 
 server.use(cors());
 server.use(express.json());
-
+server.use("/orders", orders);
 
 server.listen(PORT, () => {
   console.log(`The server is running on ${PORT}`);
