cc925ca71f0e45d0a7cb77185fc66bdf9029ebbf
AaronGulman <81993286+AaronGulman@users.noreply.github.com>
2024-04-23 20:01:45 -0500

    Integrate Header and Greeting components into App

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,16 @@
-function App() {
+import Header from "./Header/Header.jsx";
+import Greeting from "./Greeting/Greeting";
 
+function App() {
   return (
     <>
-
+      <section></section>
+      <Header />
+      <section>
+        <Greeting />
+      </section>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
