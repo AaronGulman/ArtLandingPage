16b1e029faa53f768e0491e62d9d8fcdfab89302
AaronGulman <81993286+AaronGulman@users.noreply.github.com>
2024-04-23 20:02:42 -0500

    Properly style every existing file

--- client/src/Greeting/Greeting.jsx
+++ client/src/Greeting/Greeting.jsx
@@ -1,29 +1,32 @@
 // import React, {useEffect} from 'react'
 
+import style from "./Greeting.module.css";
 
-import style from "./Greeting.module.css"
+// import image from '../assets/image.jpg'
 
-// import female from '../assets/female.jpg'
+function Header() {
+  return (
+    <div className={style.body}>
+      <div>
+        <h1 className={style.header}>Hello!</h1>
+        <p className={style.mainParagraph}>
+          Step into <span className={style.span}>my world</span>, <br></br>where{" "}
+          <span className={style.span}>artistry</span> meets{" "}
+          <span className={style.span}>experience</span>. <br></br>
+        </p>
+        <p className={style.paragraph}>
+          With 1000 years of passion and a formal art diploma, <br></br>each
+          stroke tells a story, each piece a journey. <br></br>Welcome to my
+          creative sanctuary.
+        </p>
+        <button className={style.button} type="submit" href="#order">
+          ORDER NOW
+        </button>
+      </div>
 
-function Header(){
-
-
-	return(
-		<div className={style.body}>
-			<div>
-			<h1 className={style.header}>Hello!</h1>
-				<p className={style.mainParagraph}>
-				Step into <span className={style.span}>my world</span>, <br></br>where  <span className={style.span}>artistry</span> meets  <span className={style.span}>experience</span>. <br></br></p>
-				<p className={style.paragraph}>With 1000 years of passion and a formal art diploma, <br></br>each stroke tells a story, each piece a journey. <br></br>Welcome to my creative sanctuary.
-				</p>
-				<button className={style.button} type="submit" href="#order">ORDER NOW</button>
-			</div>
-
-			{/* <img className={style.image} src={female} id="female" alt="female" height="450" width="350"/> */}
-
-		</div>
-	)
+      {/* <img className={style.image} src={image} id="image" alt="image" height="450" width="350"/> */}
+    </div>
+  );
 }
-
 
 export default Header;
--- client/src/header/Header.jsx
+++ client/src/header/Header.jsx
@@ -1,22 +1,36 @@
+import style from "./Header.module.css";
 
-import style from "./Header.module.css"
-
-function Header(){
-	return(
-	<header className={style.header}>
-	<h1 className={style.pageName}>Landing Page</h1>
-		<nav className={style.nav}>
-			<ul className={style.ul}>
-				<li className={style.list}><a className={style.links} href="#projects">Projects</a></li>
-				<li className={style.list}><a className={style.links} href="#offers">Offers</a></li>
-				{/* <li className={style.list}><a className={style.links} href="#">Contact</a></li> */}
-				<li className={style.list}><a className={style.links} href="#contact">Contact</a></li>
-				<li className={style.contact}><a className={style.links} href="#order">Order</a></li>
-			</ul>
-		</nav>
-	</header>
-	)
+function Header() {
+  return (
+    <header className={style.header}>
+      <h1 className={style.pageName}>Landing Page</h1>
+      <nav className={style.nav}>
+        <ul className={style.ul}>
+          <li className={style.list}>
+            <a className={style.links} href="#projects">
+              Projects
+            </a>
+          </li>
+          <li className={style.list}>
+            <a className={style.links} href="#offers">
+              Offers
+            </a>
+          </li>
+          {/* <li className={style.list}><a className={style.links} href="#">Contact</a></li> */}
+          <li className={style.list}>
+            <a className={style.links} href="#contact">
+              Contact
+            </a>
+          </li>
+          <li className={style.contact}>
+            <a className={style.links} href="#order">
+              Order
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
-
 
 export default Header;
