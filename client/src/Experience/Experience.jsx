7cf468cc25da517f28f3f142e4a56af91c83fbc2
AaronGulman <81993286+AaronGulman@users.noreply.github.com>
2024-04-25 20:07:27 -0500

    Add Experience section and style it

---
+++ client/src/Experience/Experience.jsx
@@ -0,0 +1,28 @@
+import style from './Experience.module.css'
+
+
+
+function Experience(){
+	return(
+		<div className={style.body}>
+			<div className={style.innerBox}>
+		
+			<h2 className={style.header}>Digital Art</h2>
+			</div>
+
+			<div className={style.innerBox}>
+		
+			<h2 className={style.header}>Modern Design</h2>
+			</div>
+
+			<div className={style.innerBox}>
+		
+			<h2 className={style.header}>Personalized</h2>
+			</div>
+
+		</div>
+	)
+}
+
+
+export default Experience;
