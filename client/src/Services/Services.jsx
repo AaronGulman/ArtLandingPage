8cbf42b1065a7508a27bae0e7e3c351083dc65ff
AaronGulman <81993286+AaronGulman@users.noreply.github.com>
2024-04-26 12:55:22 -0500

    Add Services component and style it

---
+++ client/src/Services/Services.jsx
@@ -0,0 +1,13 @@
+import style from "./Services.module.css"
+
+function Services(){
+	return(
+		<div className={style.projectBox}>
+			<h1 className={style.header}>Projects made by <span className={style.span}>ME</span></h1>
+			<p className={style.mainParagraph}>Discover my <span className={style.span}>diverse portfolio</span>, a kaleidoscope of <span className={style.span}>artistic</span> endeavors<br></br> that showcases the depth and breadth of my <span className={style.span}>creative vision.</span> </p>
+		</div>
+	)
+}
+
+
+export default Services;
