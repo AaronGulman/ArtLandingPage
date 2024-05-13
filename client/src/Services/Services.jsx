3bb3714dfb801d1617a78e7299ee91fa23704bf8
AaronGulman <81993286+AaronGulman@users.noreply.github.com>
2024-05-12 23:03:49 -0400

    Refactor Services component and import CSS styles

--- client/src/Services/Services.jsx
+++ client/src/Services/Services.jsx
@@ -1,13 +1,19 @@
-import style from "./Services.module.css"
+import style from "./Services.module.css";
 
-function Services(){
-	return(
-		<div className={style.projectBox}>
-			<h1 className={style.header}>Projects made by <span className={style.span}>ME</span></h1>
-			<p className={style.mainParagraph}>Discover my <span className={style.span}>diverse portfolio</span>, a kaleidoscope of <span className={style.span}>artistic</span> endeavors<br></br> that showcases the depth and breadth of my <span className={style.span}>creative vision.</span> </p>
-		</div>
-	)
+function Services() {
+  return (
+    <div className={style.projectBox}>
+      <h1 className={style.header}>
+        Projects made by <span className={style.span}>ME</span>
+      </h1>
+      <p className={style.mainParagraph}>
+        Discover my <span className={style.span}>diverse portfolio</span>, a
+        kaleidoscope of <span className={style.span}>artistic</span> endeavors
+        <br></br> that showcases the depth and breadth of my{" "}
+        <span className={style.span}>creative vision.</span>{" "}
+      </p>
+    </div>
+  );
 }
-
 
 export default Services;
