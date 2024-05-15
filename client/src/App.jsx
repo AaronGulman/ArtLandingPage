7c1c8435c8e47a0a685ca8371bcb7989e5c7c724
AaronGulman <81993286+AaronGulman@users.noreply.github.com>
2024-05-14 21:08:28 -0400

    Apply proper  JSX formatting

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,33 +1,31 @@
-import Header from './Header/Header'
-import Greeting from './Greeting/Greeting'
-import Experience from './Experience/Experience'
-import Projects from './Projects/Projects'
-import Services from './Services/Services'
-
-
+import Header from "./Header/Header";
+import Greeting from "./Greeting/Greeting";
+import Experience from "./Experience/Experience";
+import Projects from "./Projects/Projects";
+import Services from "./Services/Services";
 
 function App() {
   return (
     <>
-    <section>
-    <Header/>
-    </section>
+      <section>
+        <Header />
+      </section>
 
-    <section>   
-      <Greeting/>
-    </section>
-    
-    <section>
-    <Experience/>
-    </section>
+      <section>
+        <Greeting />
+      </section>
 
-    <section id="projects">
-    <Projects/>
-    </section>
+      <section>
+        <Experience />
+      </section>
 
-    <section id="Services">
-    <Services/>
-    </section>
+      <section id="projects">
+        <Projects />
+      </section>
+
+      <section id="Services">
+        <Services />
+      </section>
     </>
   );
 }
