b4f058ff1df9e8912edbdb27d57564ad3de1577e
AaronGulman <81993286+AaronGulman@users.noreply.github.com>
2024-05-19 23:55:03 -0400

    Properly format the documents

--- client/src/Projects/Projects.jsx
+++ client/src/Projects/Projects.jsx
@@ -4,29 +4,29 @@ function Projects() {
   return (
     <div className={style.projectBox}>
       <div>
-      <div className={style.intro}>
-        <h1 className={style.header}>
-          Projects made by <span className={style.span}>ME</span>
-        </h1>
-      </div>
-      <div className={style.introTwo}></div>
-      <p className={style.mainParagraph}>
-        Discover my <span className={style.span}>diverse portfolio</span>, a
-        kaleidoscope of <span className={style.span}>artistic</span> endeavors
-        <br></br> that showcases the depth and breadth of my{" "}
-        <span className={style.span}>creative vision.</span>{" "}
-      </p>
-      <div className={style.projects}>
-        <div className={style.innerBox}></div>
-        <hr className={style.hr} />
-        <div className={style.innerBox}></div>
-        <hr className={style.hr} />
+        <div className={style.intro}>
+          <h1 className={style.header}>
+            Projects made by <span className={style.span}>ME</span>
+          </h1>
+        </div>
+        <div className={style.introTwo}></div>
+        <p className={style.mainParagraph}>
+          Discover my <span className={style.span}>diverse portfolio</span>, a
+          kaleidoscope of <span className={style.span}>artistic</span> endeavors
+          <br></br> that showcases the depth and breadth of my{" "}
+          <span className={style.span}>creative vision.</span>{" "}
+        </p>
+        <div className={style.projects}>
+          <div className={style.innerBox}></div>
+          <hr className={style.hr} />
+          <div className={style.innerBox}></div>
+          <hr className={style.hr} />
 
-        <div className={style.innerBox}></div>
-        <hr className={style.hr} />
+          <div className={style.innerBox}></div>
+          <hr className={style.hr} />
 
-        <div className={style.innerBox}></div>
-      </div>
+          <div className={style.innerBox}></div>
+        </div>
       </div>
     </div>
   );
