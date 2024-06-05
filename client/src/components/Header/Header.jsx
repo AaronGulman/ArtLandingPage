5ddfe8a5c9081df4611b309c4e8e4a18b1e5d2b9
AaronGulman <81993286+AaronGulman@users.noreply.github.com>
2024-06-04 23:48:35 -0400

    Refactor Header component to include dynamic order visibility toggle

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,9 +1,19 @@
+import React, { useState } from "react";
 import style from "./Header.module.css";
+import Order from "../Order/Order";
 
 function Header() {
+  const [isOrderVisible, setIsOrderVisible] = useState(false);
+
+  const toggleOrderVisibility = () => {
+    setIsOrderVisible((prevVisible) => !prevVisible);
+  };
+
   return (
     <div className={style.header}>
-      <h1 className={style.pageName}><img height={110} width={200} src="../../public/NewLogo.svg"/></h1>
+      <h1 className={style.pageName}>
+        <img height={110} width={200} src="../../public/NewLogo.svg" alt="Logo" />
+      </h1>
       <nav className={style.nav}>
         <ul className={style.ul}>
           <li className={style.list}>
@@ -16,19 +26,19 @@ function Header() {
               Services
             </a>
           </li>
-          {/* <li className={style.list}><a className={style.links} href="#">Contact</a></li> */}
           <li className={style.list}>
             <a className={style.links} href="#contact">
               Contact
             </a>
           </li>
           <li className={style.contact}>
-            <a className={style.links} href="#order">
+            <a onClick={toggleOrderVisibility} className={style.links} href="#order">
               Order
             </a>
           </li>
         </ul>
       </nav>
+      {isOrderVisible && <Order />}
     </div>
   );
 }
