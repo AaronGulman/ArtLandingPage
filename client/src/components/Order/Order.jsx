15a7e83436eb4d5e05d6478bc4130edd9e437d3a
AaronGulman <81993286+AaronGulman@users.noreply.github.com>
2024-06-05 21:18:58 -0400

    Initial structure of the form element

--- client/src/components/Order/Order.jsx
+++ client/src/components/Order/Order.jsx
@@ -3,6 +3,21 @@ import style from './Order.module.css'
 const Order = () => {
   return (
     <form className={style.body}>
+      <h1>ORDER NOW</h1>
+      <label>Name: </label>
+      <input type="text" />
+      <label>Phone number: </label>
+      <input type='tel' />
+      <label>Email: </label>
+      <input type="email" />
+      <label>Preferred means of communication:</label>
+      <div>
+      <input type="checkbox" name="" id=""/>WhatsApp
+      <input type="checkbox" name="" id=""/>SMS
+      <input type="checkbox" name="" id=""/>Email
+      <input type="checkbox" name="" id=""/>Social Media
+      </div>
+      <input type="submit" value="SUBMIT" />
 
       
     </form>
