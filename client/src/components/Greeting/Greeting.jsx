8cf47c2d981cd3cd02343092fa3ff16b9365a3ff
AaronGulman <81993286+AaronGulman@users.noreply.github.com>
2024-06-16 09:29:35 -0400

    Change h1 text on the main page and reposition the elements

--- client/src/components/Greeting/Greeting.jsx
+++ client/src/components/Greeting/Greeting.jsx
@@ -8,17 +8,17 @@ function Greeting() {
   return (
     <div className={style.body}>
       <div>
-        <h1 className={style.header}>Hello!</h1>
-        <p className={style.mainParagraph}>
-          Write<span className={style.span}>something</span>, <br></br>inspirational{" "}
-          <span className={style.span}>in</span> this{" "}
-          <span className={style.span}>section</span>. <br></br>
-        </p>
-        <p className={style.paragraph}>(Write about your experience here)</p>
-        <button className={style.button} type="submit" href="#order">
-          ORDER NOW
-        </button>
+      <h1 className={style.header}>Hello,</h1>
+      <p className={style.mainParagraph}>
+        Write<span className={style.span}> something</span>, <br></br>
+        inspirational <span className={style.span}> in </span> this{" "}
+        <span className={style.span}> section </span>. <br></br>
+      </p>
+      <p className={style.paragraph}>(Write about your experience here)</p>
       </div>
+      <button className={style.button} type="submit" href="#order">
+        ORDER NOW
+      </button>
     </div>
   );
 }
