63970ef766787709d7d293d26d6ecc7ee793dc9c
AaronGulman <81993286+AaronGulman@users.noreply.github.com>
2024-06-20 23:01:00 -0400

    Add Cards to Services.jsx

--- client/src/components/Services/Services.jsx
+++ client/src/components/Services/Services.jsx
@@ -1,17 +1,22 @@
+import { useState } from "react";
+import Card from "../Card/Card";
 import style from "./Services.module.css";
 
 function Services() {
+
   return (
-    <div className={style.projectBox}>
+    <div className={style.servicesBox}>
       <h1 className={style.header}>
-        Projects made by <span className={style.span}>(your name)</span>
+        Here you<span className={style.span}> write what </span>
+        services you provide
       </h1>
-      <p className={style.mainParagraph}>
-        Write <span className={style.span}>about</span>, a
-       the <span className={style.span}>projects</span> that
-        <br></br> you have worked on{" "}
-        <span className={style.span}>here..</span>{" "}
-      </p>
+      <div className={style.cards}>
+      <Card price="999"/>
+      <Card/>
+      <Card/>
+      <Card/>
+
+      </div>
     </div>
   );
 }
