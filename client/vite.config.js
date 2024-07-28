2cd9e974434fd5d659dca781682b47095a853fac
AaronGulman <81993286+AaronGulman@users.noreply.github.com>
2024-07-28 12:56:24 -0400

    Add plugins:[react()] and keepthe base in the config

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,7 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: 'ArtLandingPage', // replace with your actual GitHub repository name
+  plugins: [react()],
+  base: '/ArtLandingPage', // replace with your actual GitHub repository name
 });
 
 
