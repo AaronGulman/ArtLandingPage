a46cfcb2c76805a02e598ea60eb21d79e8aba19b
AaronGulman <81993286+AaronGulman@users.noreply.github.com>
2024-07-30 23:00:45 -0400

    Refactor Order.jsx:
    1) Corrected input values to use the appropriate state variables (`name`, `email`, `telephone`);
    
    2) Fixed checkbox inputs to use state management with `onClick` handlers;
    
    3) Updated label texts for consistency (e.g., "Phone number" to "Number");
    
    4) Replaced `<submit>` element with a `<button>` element for proper form submission behavior.

--- client/src/components/Order/Order.jsx
+++ client/src/components/Order/Order.jsx
@@ -10,32 +10,52 @@ const Order = () => {
   return (
     <div className={style.body}>
       <h1>ORDER NOW</h1>
-      <label>Name: </label>
-      <input type="text" value={clientName} />
-      <label>Phone number: </label>
-      <input type="tel" />
-      <label>Email: </label>
-      <input type="email" />
+      <label>Name:</label>
+
+        <input type="text" value={name} />
+
+      <label>Number:</label>
+        <input type="tel" value={telephone}/>
+      <label>Email:</label>
+        <input type="email" value={email}/>
       <label>Preferred Contact Method:</label>
       <div>
         <label>
-          <input type="checkbox" />
+          <input
+            type="checkbox"
+            value={name}
+            onClick={(e) => setName(e.target.value)}
+          />
           WhatsApp
         </label>
         <label>
-          <input type="checkbox" />
+          <input
+            type="checkbox"
+            value={email}
+            onClick={(e) => setEmail(e.target.value)}
+          />
           SMS
         </label>
         <label>
-          <input type="checkbox" />
+          <input
+            type="checkbox"
+            value={telephone}
+            onClick={(e) => setTelephone(e.target.value)}
+          />
           Email
         </label>
         <label>
-          <input type="checkbox" />
+          <input
+            type="checkbox"
+            value={communication}
+            onClick={(e) => setCommunication(e.target.value)}
+          />
           Social Media
         </label>
       </div>
-      <submit className={style.submit} value="SUBMIT">SUBMIT</submit>
+      <submit className={style.submit} value="SUBMIT">
+        SUBMIT
+      </submit>
     </div>
   );
 };
